42181ee171f0ab1d6bad62adb7bf8d8244b9238b
K1BERSTALKER <xabibullaevxayotullo3211@gmail.com>
2024-06-11 22:23:55 +0000

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Footer, Navbar, TodoList } from "./layout";
 
 function App() {
   return (
-    <div className="max-w-7xl mx-auto border border-white h-full">
+    <div className="max-w-7xl mx-auto  h-full">
       <Navbar />
       <TodoList />
       <Footer />
